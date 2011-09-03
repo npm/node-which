e7b41e1833b0d0f36e6074789fd5caf1adbaf403
isaacs <i@izs.me>
2011-09-02 17:13:48 -0700

    Respect the PATHEXT env for win32 support

--- which.js
+++ which.js
@@ -15,23 +15,37 @@ try {
 // console.log(process.argv)
 
 function isExe (mod, uid, gid) {
-  return (mod & 0001)
-      || (mod & 0010) && gid === process.getgid()
-      || (mod & 0100) && uid === process.getuid()
+  //console.error("isExe?", (mod & 0111).toString(8))
+  var ret = (mod & 0001)
+      || (mod & 0010) && process.getgid && gid === process.getgid()
+      || (mod & 0100) && process.getuid && uid === process.getuid()
+  //console.error("isExe?", ret)
+  return ret
 }
 function which (cmd, cb) {
   if (cmd.charAt(0) === "/") return cb(null, cmd)
   var pathEnv = (process.env.PATH || "").split(COLON)
+    , pathExt = [""]
+  if (process.platform === "win32") {
+    pathEnv.push(process.cwd())
+    pathExt = (process.env.PATHEXT || ".EXE").split(COLON)
+  }
+  //console.error("pathEnv", pathEnv)
   ;(function F (i, l) {
     if (i === l) return cb(new Error("not found: "+cmd))
-    var p = path.join(pathEnv[i], cmd)
-    if (p === process.execPath) return cb(null, p)
-    fs.stat(p, function (er, stat) {
-      if (!er && stat && isExe(stat.mode, stat.uid, stat.gid)) {
-        return cb(null, p)
-      }
-      return F(i+1, l)
-    })
+    var p = path.resolve(pathEnv[i], cmd)
+    ;(function E (ii, ll) {
+      if (ii === ll) return F(i + 1, l)
+      var ext = pathExt[ii]
+      //console.error(p + ext)
+      fs.stat(p + ext, function (er, stat) {
+        if (!er && stat && isExe(stat.mode, stat.uid, stat.gid)) {
+          //console.error("yes, exe!", p + ext)
+          return cb(null, p + ext)
+        }
+        return E(ii + 1, ll)
+      })
+    })(0, pathExt.length)
   })(0, pathEnv.length)
 }
 
