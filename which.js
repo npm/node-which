e454ee431a42842f4603613b2369f933d294c0cb
isaacs <i@izs.me>
2015-05-09 21:03:39 -0700

    use single-quotes

--- which.js
+++ which.js
@@ -1,13 +1,13 @@
 module.exports = which
 which.sync = whichSync
 
-var path = require("path")
-var COLON = process.platform === "win32" ? ";" : ":"
+var path = require('path')
+var COLON = process.platform === 'win32' ? ';' : ':'
 var isExe
-var fs = require("fs")
+var fs = require('fs')
 var isAbsolute = require('is-absolute')
 
-if (process.platform == "win32") {
+if (process.platform == 'win32') {
   // On windows, there is no good way to check that a file is executable
   isExe = function isExe () { return true }
 } else {
@@ -27,23 +27,23 @@ function which (cmd, opt, cb) {
   }
 
   var colon = opt.colon || COLON
-  var pathEnv = (opt.path || process.env.PATH || "").split(colon)
-  var pathExt = [""]
+  var pathEnv = (opt.path || process.env.PATH || '').split(colon)
+  var pathExt = ['']
 
-  if (process.platform === "win32") {
+  if (process.platform === 'win32') {
     pathEnv.push(process.cwd())
-    pathExt = opt.pathExt || (process.env.PATHEXT || ".EXE").split(colon)
-    if (cmd.indexOf(".") !== -1)
-      pathExt.unshift("")
+    pathExt = opt.pathExt || (process.env.PATHEXT || '.EXE').split(colon)
+    if (cmd.indexOf('.') !== -1)
+      pathExt.unshift('')
   }
 
   // If it's absolute, then we don't bother searching the pathenv.
   // just check the file itself, and that's it.
   if (isAbsolute(cmd))
-    pathEnv = [""]
+    pathEnv = ['']
 
   ;(function F (i, l) {
-    if (i === l) return cb(new Error("not found: "+cmd))
+    if (i === l) return cb(new Error('not found: '+cmd))
     var p = path.resolve(pathEnv[i], cmd)
     ;(function E (ii, ll) {
       if (ii === ll) return F(i + 1, l)
@@ -66,21 +66,21 @@ function whichSync (cmd, opt) {
 
   var colon = opt.colon || COLON
 
-  var pathEnv = (opt.path || process.env.PATH || "").split(colon)
-  var pathExt = [""]
+  var pathEnv = (opt.path || process.env.PATH || '').split(colon)
+  var pathExt = ['']
 
-  if (process.platform === "win32") {
+  if (process.platform === 'win32') {
     pathEnv.push(process.cwd())
-    pathExt = (opt.pathExt || process.env.PATHEXT || ".EXE").split(colon)
-    if (cmd.indexOf(".") !== -1) {
-      pathExt.unshift("")
+    pathExt = (opt.pathExt || process.env.PATHEXT || '.EXE').split(colon)
+    if (cmd.indexOf('.') !== -1) {
+      pathExt.unshift('')
     }
   }
 
   // If it's absolute, then we don't bother searching the pathenv.
   // just check the file itself, and that's it.
   if (isAbsolute(cmd))
-    pathEnv = [""]
+    pathEnv = ['']
 
   for (var i = 0, l = pathEnv.length; i < l; i ++) {
     var p = path.join(pathEnv[i], cmd)
@@ -95,5 +95,5 @@ function whichSync (cmd, opt) {
     }
   }
 
-  throw new Error("not found: "+cmd)
+  throw new Error('not found: '+cmd)
 }
