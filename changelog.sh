801957dd826fdcd45def409026285e3f7b666b14
isaacs <i@izs.me>
2016-05-04 17:47:53 -0700

    make the changelog bulleted

--- changelog.sh
+++ changelog.sh
@@ -2,5 +2,7 @@
 (
   echo '# Changes'
   echo ''
-  git log --first-parent --pretty=format:'%s' | perl -p -e 's/^((v?[0-9]+\.?)+)$/\n## \1\n/g'
+  git log --first-parent --pretty=format:'%s' \
+    | perl -p -e 's/^((v?[0-9]+\.?)+)$/\n## \1\n/g' \
+    | perl -p -e 's/^(.[^#]*)$/* \1/g'
 )> CHANGELOG.md
